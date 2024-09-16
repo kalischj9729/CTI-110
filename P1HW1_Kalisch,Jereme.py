9468cc4392f9f381c3f8494926909786488f0844
kalischj9729 <kalischj9729@student.faytechcc.edu>
2024-09-16 11:50:08 -0400

    Add files via upload

---
+++ P1HW1_Kalisch,Jereme.py
@@ -0,0 +1,24 @@
+# Jereme Kalisch
+# 09/16/2024
+# P1HW1
+# Using Python's you will write a Python program that uses mathematical expressions.
+
+
+print('-----Calculating Exponenets-----)
+
+base_value = int(input('Enter an interger as the base value: '))
+
+# convert base_value to interger
+base_value = int(base_value)
+
+      
+# get input from user and convert to interger
+exponent = int(input('Enter an interger as the exponent: '))
+
+# convert exponent to interger
+
+exponent = int(exponent)
+
+# Display math result to user
+
+print(base_value, 'raised to the power of' exponent, 'is', base_value**exponent, '!!')
