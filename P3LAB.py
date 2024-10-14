41ccebed4538f16bf4cb75079cb358df766c5214
kalischj9729 <kalischj9729@student.faytechcc.edu>
2024-10-14 11:42:54 -0400

    Add files via upload

---
+++ P3LAB.py
@@ -0,0 +1,22 @@
+# Jereme Kalisch
+# 10/14/2024
+# P3LAB
+# Calculate the most efficient number of dollars, quarters, dimes, nickels, and pennies needed to make the given amount of money.
+
+# Enter the amount of money to be sorted.
+
+money = float(input("Enter the amount of money as a float: $"))
+
+
+# Floor division - returns integer portion
+# //
+print(100//money)
+
+# Modulo - returns ONLY the remainder
+print(100%money)
+
+
+'''
+else:
+    "No change"
+'''
