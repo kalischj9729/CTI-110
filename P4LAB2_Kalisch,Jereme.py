2284e92ca1b2b02de8b94ba311924f47f4bd1dcb
kalischj9729 <kalischj9729@student.faytechcc.edu>
2024-10-30 10:15:09 -0400

    Add files via upload

--- P4LAB2_Kalisch,Jereme.py
+++ P4LAB2_Kalisch,Jereme.py
@@ -1,16 +1,21 @@
 # Jereme Kalisch
-# 10/29/2024
+# 10/30/2024
 # P4LAB2 
 # Write a program that asks the user to enter an integer and display multiplication table.
 
 # Starts run_again so the loop starts
+
 run_again = "yes" 
+
 # Initialize run_again so the loop starts
 # Loop runs as long as the user inputs "yes"
+
 while run_again == "yes": 
     try:
         user_input = int(input("Enter an integer: "))
+
 # Using a for loop to display the table from 1 to 12
+
         if user_input >= 0:
             print(f"\nMultiplication table for {user_input}:")
             for i in range(1, 13):  
@@ -23,7 +28,9 @@ while run_again == "yes":
         print("Error: Please enter a valid integer.")
     
 # Ask if the user wants to run the program again
+
     run_again = input("Do you want to run the program again? (yes/no): ").lower()
+
 # Check for valid input (either "yes" or "no")
     while run_again not in ["yes", "no"]:
         print("Failed to give appropriate response. Please type 'yes' or 'no'.")
